e571c65ae98122bd7ed6b543e8e9bb36c4e262da
Alex <7982529+alexm622@users.noreply.github.com>
2021-09-16 12:45:29 -0400

    Argument Manager

---
+++ distrib-master/src/main/java/com/alexcomeau/utils/ArgManager.java
@@ -0,0 +1,17 @@
+package com.alexcomeau.utils;
+
+/**
+ * ArgManager
+ */
+public class ArgManager {
+
+    private String[] args;
+
+    public ArgManager(String[] args){
+        this.args = args;
+    }
+
+    public void parseArgs(){
+
+    }
+}
